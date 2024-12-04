4040169419a4d03e0678788d2e18c18cf86e131f
Nathan-C-Wong <nathanbusiness55555@gmail.com>
2024-12-04 13:14:28 -0800

    temp

--- src/main/java/ULogout.java
+++ src/main/java/ULogout.java
@@ -1,6 +1,5 @@
-
-
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,36 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ULogout
- */
+import com.studysmart.User;
+
 @WebServlet("/ULogout")
 public class ULogout extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public ULogout() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+HttpSession session = request.getSession(false); // Don't create a new session if it doesn't exist
+        
+        if (session != null) {
+            session.invalidate();  // Invalidate the session, clearing all session attributes
+        }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-        session.setAttribute("username", "");
+        // Redirect the user to the login page after invalidation
         response.sendRedirect("loginPage.jsp");
-	}
-
+    }
 }
